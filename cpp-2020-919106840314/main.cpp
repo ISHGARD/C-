fd0561c9bb48df832bfd98f1850ddad47aae6e48
ISHGARD <70511062+ISHGARD@users.noreply.github.com>
2020-08-31 20:58:38 +0800

    Add files via upload
    大致设计界面

---
+++ cpp-2020-919106840314/main.cpp
@@ -0,0 +1,14 @@
+#include "mainwindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+
+    w.setWindowTitle(QString("计算器"));
+
+    w.show();
+
+    return a.exec();
+}
